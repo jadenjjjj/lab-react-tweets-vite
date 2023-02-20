2f4475b2e05ce07d2acbcf976d755a6a44b2f841
Dengxian ji <99891555+jadenjjjj@users.noreply.github.com>
2023-02-20 16:27:46 +0000

    done

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ProfileImage(props) {
+    const {  image } = props;
+
+    return <img src="https://res.cloudinary.com/dqqdwbrhs/image/upload/v1676580859/tomatoes_ebc6mv.jpg" className='profile' alt='profile' />
+}
+
+
+export default ProfileImage;
